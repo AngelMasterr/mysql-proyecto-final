657a2746cde5457d53a934faf2482a7c5ef3b5c7
AngelMasterr <angelduarteprog@gmail.com>
2024-01-04 20:03:30 -0500

    actualizando las funciones random

--- Ejemplo de funcion RAND.sql
+++ Ejemplo de funcion RAND.sql
@@ -5,8 +5,11 @@
 SELECT (rand() * (250-20+1)) + 20 AS ALEATORIO;
 SELECT FLOOR((rand() * (250-20+1)) + 20) AS ALEATORIO;
 
+# permite que los usuarios con permisos adecuados puedan crear funciones y procedimientos
 SET GLOBAL log_bin_trust_function_creators = 1;
 
 SELECT f_aleatorio(1, 10) AS RESULTADO;
 
 SELECT f_cliente_aleatorio() AS CLIENTE;
+
+SELECT `f_producto_aleatorio`() AS CLIENTE;
